0c477f33e668bef51c884675b81e233c11eff8a8
MK-coder <52727959+MK-coder@users.noreply.github.com>
2019-07-24 22:08:00 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,8 @@
+import {Song} from '../../app/song';
+
+export const MYSONGS: Song[] = [
+    {name: "I'm Sorry", artist: 'Joyner Lucas', genre: 'Rap', yearReleased: 2017, picture: '../../assets/images/1.jpg'},
+    {name: 'High Hopes', artist: 'Panic! At The Disco', genre: 'Pop Rock', yearReleased: 2018, picture: '../../assets/images/2.jpg'},
+    {name: 'Castle On The Hill', artist: 'Ed Sheeran', genre: 'Folk Pop', yearReleased: 2017, picture: '../../assets/images/3.jpg'},
+    {name: 'Love Me', artist: 'Lights & Felix Cartel', genre: 'Electronic', yearReleased: 2019, picture: '../../assets/images/4.jpg'}
+];
